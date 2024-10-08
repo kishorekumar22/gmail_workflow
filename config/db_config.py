e649fbc8d8f9e142759cf93ee3b127eb0935c2cf
Kishorekumar Shanmugam <31817755+kishorekumar22@users.noreply.github.com>
2024-10-08 19:24:25 +0530

    Documentation added

--- config/db_config.py
+++ config/db_config.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
+# All the databases configurations goes in here, contains the db sessions and engines.
+
 engine = create_engine("sqlite:///emails_automator.sqlite")
 conn = engine.connect()
 
